eb0fd7d2d04e8cb2bbf43044a0043980421824bf
Esteban Mendoza <estemendoza@gmail.com>
2014-08-01 19:56:11 -0300

    Added setup.py for an easy python distribution

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='traviata',
+      version='0.1',
+      description='Javascript automation with Python and Selenium',
+      author='Esteban Mendoza',
+      author_email='estemendoza@gmail.com',
+      url='https://github.com/estemendoza/traviata',
+      py_modules=['traviata'],
+      )
